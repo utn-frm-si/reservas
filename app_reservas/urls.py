4c0a351b9cac9df4eda6a30db03309834fae2562
adamantike <mike@fmanganiello.com.ar>
2015-10-11 03:09:25 -0300

    Creación de plantilla inicial para aulas

---
+++ app_reservas/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^cuerpo_(?P<num_cuerpo>[0-9]+)/nivel_(?P<num_nivel>[0-9]+)/aula_(?P<num_aula>[0-9]+)/$', views.aula_detalle, name='aula_detalle'),
+]
--- reservas/urls.py
+++ reservas/urls.py
@@ -17,5 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^reservas/', include('app_reservas.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
