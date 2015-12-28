edbbe78950ffc8bc0c813ce6ae91eec9274186bd
adamantike <mike@fmanganiello.com.ar>
2015-12-28 09:00:24 -0300

    Espera de inicialización de PostgreSQL, desde el contenedor Django

--- run-django.sh
+++ run-django.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+# Función que espera a que PostgreSQL haya sido inicializado.
+function pg_wait {
+  while : ; do
+    pg_isready --host db --user postgres
+    sleep 5
+    if [ $? == 0 ]; then
+      break
+    else
+      sleep 0.1
+    fi
+  done
+}
+
+pg_wait
+
 python manage.py migrate
 python manage.py bower install -- --allow-root
 python manage.py collectstatic --noinput
