fbe2835b3e12507d85498cc53f92b95f24a6f410
adamantike <mike@fmanganiello.com.ar>
2015-12-31 20:33:40 -0300

    Mejora en el armado de eventos en formato JSON

--- app_reservas/models/Aula.py
+++ app_reservas/models/Aula.py
@@ -1,5 +1,7 @@
 # coding=utf-8
 
+import json
+
 from django.db import models
 
 from app_reservas.adapters.google_calendar import obtener_eventos
@@ -40,10 +42,12 @@ class Aula(Recurso):
                 primera_iteracion = False
             else:
                 eventos_json += ','
-            evento_str = '{"title": "' + evento['titulo'] + '", ' + \
-                         '"start": "' + evento['inicio_str'] + '", ' + \
-                         '"end": "' + evento['fin_str'] + '", ' + \
-                         '"resourceId": "' + str(self.id) + '"}'
+            evento_str = json.dumps({
+                'title': evento['titulo'],
+                'start': evento['inicio_str'],
+                'end': evento['fin_str'],
+                'resourceId': str(self.id)
+            })
             eventos_json += evento_str
         eventos_json += ']'
         return eventos_json
