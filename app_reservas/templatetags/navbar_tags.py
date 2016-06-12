f605303e77a0ab8420b998c109483616b34a4134
adamantike <mike@fmanganiello.com.ar>
2016-06-12 01:23:33 -0300

    Creación de enlace de 'Videoconferencias' para ALI

--- app_reservas/templatetags/navbar_tags.py
+++ app_reservas/templatetags/navbar_tags.py
@@ -4,6 +4,7 @@ from django import template
 
 from ..models import (
     Area,
+    Aula,
     Cuerpo,
     TipoLaboratorio,
 )
@@ -14,9 +15,17 @@ register = template.Library()
 
 @register.inclusion_tag('app_reservas/navbar.html')
 def obtener_informacion_navbar():
+    # Obtiene la instancia de la sala de videoconferencias.
+    sala_videoconferencias = Aula.objects.filter(
+        nivel__cuerpo__numero=6,
+        nivel__numero=2,
+        numero=1,
+    ).first()
+
     context = {
         'lista_cuerpos': Cuerpo.objects.all(),
         'lista_areas': Area.objects.all(),
         'lista_tipos_laboratorio': TipoLaboratorio.objects.all(),
+        'sala_videoconferencias': sala_videoconferencias,
     }
     return context
